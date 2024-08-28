30f7a4ce15140f7ebbaa83009a3aedba48e49a24
BeniKiambote10 <benikiambote10@gmail.com>
2024-08-28 02:39:10 -0400

    worked on the navbar and footer and added components and pages

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Home from "./Pages/HomePage";
+import About from "./Pages/AboutPage";
+import Volunteer from "./Pages/VolunterPage"; // Ensure this matches your actual file name
+import Donation from "./Pages/DonationPage";
+import Contact from "./Pages/ContactPage";
+import Menu from "./Components/Menu";
 
 function App() {
+  const [clicked, isClicked] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar clicked={clicked} isClicked={isClicked} />
+      {clicked ? <Menu /> : null}
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/volunteer" element={<Volunteer />} />
+          <Route path="/donation" element={<Donation />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
