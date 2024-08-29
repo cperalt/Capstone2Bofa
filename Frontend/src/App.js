0abbe9788b1456a876c68f8f1c91753d667b81ec
Birhanu C kramer <Birhanuckramer@Administrators-MacBook-Pro.local>
2024-08-29 13:39:58 -0400

    upated app.js file along with fixing linking in pages and installed password hashing

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
-import Home from "./pages/HomePage";
-import About from "./pages/AboutPage";
-import Volunteer from "./pages/VolunterPage"; // Ensure this matches your actual file name
-import Donation from "./pages/DonationPage";
-import Contact from "./pages/ContactPage";
+import Home from "./Pages/HomePage";
+import About from "./Pages/AboutPage";
+import Volunteer from "./Pages/VolunterPage"; // Ensure this matches your actual file name
+import Donation from "./Pages/DonationPage";
+import Register from "./Pages/Register"; //file path is correct
+import Login from './Pages/Login';
+import Contact from "./Pages/ContactPage";
 import Menu from "./Components/Menu";
 
 function App() {
@@ -23,6 +25,8 @@ function App() {
           <Route path="/volunteer" element={<Volunteer />} />
           <Route path="/donation" element={<Donation />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Login" element={<Login/>} />
         </Routes>
       </main>
       <Footer />
